d951093c5e413928b1a9074f214afdda2ade41aa
Aleksander Lukashov <lukashov784@gmail.com>
2024-12-05 18:57:43 +0700

    third: catalog and pagination are ready

--- goods/views.py
+++ goods/views.py
@@ -13,9 +13,27 @@ class CategoryView(ListView):
         return context
 
 
-
 class ProductView(DetailView):
     ...
 
-class CatalogView(DetailView):
-    ...
+
+class CatalogView(ListView):
+    model = Product
+    template_name = 'goods/catalog.html'
+    context_object_name = 'products'
+    slug_url_kwarg = 'catalog_slug'
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'PC-Shop - Catalog'
+        return context
+
+    def get_queryset(self):
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        if slug == 'all':
+            goods = Product.objects.all()
+        else:
+            goods = Product.objects.filter(category__slug=slug)
+        return goods
+
